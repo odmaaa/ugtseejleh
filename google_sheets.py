bf88de60f5e21e0e9acfc5aa439f60e9038de067
omkaaa <odmaa_b@hotmail.com>
2019-12-25 17:37:26 +0800

    google_sheets full path problem

--- google_sheets.py
+++ google_sheets.py
@@ -15,6 +15,12 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 # SAMPLE_SPREADSHEET_ID = '14mY0zLrTJRPEJTprBen3FOqw1MTjM875qHiL_YHl0TA'
 # SAMPLE_RANGE_NAME = '新HSK5000词2!A:O'
 
+
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
+token_pickle = os.path.join(PROJECT_ROOT, 'token.pickle')
+credentials = os.path.join(PROJECT_ROOT, 'credentials.json')
+
+
 def main(SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME):
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
@@ -23,8 +29,8 @@ def main(SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME):
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(token_pickle):
+        with open(token_pickle, 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -32,10 +38,10 @@ def main(SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                credentials, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(token_pickle, 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
